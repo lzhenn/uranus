65935797ff25094a525e4149bac3f5b7c4791156
Novarizark <novarizark@gmail.com>
2023-09-08 16:15:46 +0800

    first commit

---
+++ ReadMe.md
@@ -0,0 +1,7 @@
+# Uranus 
+
+Uranus is the central controllor of the NJORD and AEGIR systems
+
+
+
+**Any question, please contact Zhenning LI (zhenningli91@gmail.com)**
---
+++ uranus/lib/io.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""specific module for IO"""
+# ---imports---
+
+
+# ---Module regime consts and variables---
+print_prefix='lib.io>>'
+
+
+# ---Classes and Functions---
+
+
+# ---Unit test---
+if __name__ == '__main__':
+    pass
